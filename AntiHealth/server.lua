8f42b09d53fc1b0648fbabc3c1e0369bd1fff51e
Dev-Shaz <63104021+Dev-Shaz@users.noreply.github.com>
2021-05-04 16:44:31 +0100

    Update server.lua

--- AntiHealth/server.lua
+++ AntiHealth/server.lua
@@ -1,4 +1,4 @@
-local webhook = "https://discord.com/api/webhooks/826389383258308608/6z414doeLJWinoiujOKJTnaxGl3lbTU8qM1oMfCskg0zAJsCFdH-_ZnruuX8fa8FOsys"
+local webhook = "https://discord.com/api/webhooks/826389383258308608/6z414doeLJWinoiujOKJTnaxGl3lbTU8qM1oMfCskg0zAJsCFdH-_ZnruuX8fa8FOsys"   --- PUT YOUR WEBHOOK HERE
 RegisterServerEvent("Anticheat:health")
 AddEventHandler("Anticheat:health", function(a)
     local amount = a
@@ -15,5 +15,5 @@ AddEventHandler("Anticheat:health", function(a)
 
     PerformHttpRequest(webhook, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
 
-    DropPlayer(source, "[Anticheat] Health added")
+    DropPlayer(source, "[Anticheat] Health added") -- you might want to change to ban trigger ¯\_(ツ)_/¯
 end)
